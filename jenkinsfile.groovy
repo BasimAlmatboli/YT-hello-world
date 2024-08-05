4fa9ff8340345054baebbc5fc2e1bd397b431f31
Basim Almatboli <47425179+BasimAlmatboli@users.noreply.github.com>
2024-08-05 18:02:28 +0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -17,30 +17,31 @@ pipeline {
             }
         }
         
-        stage('Clean Up S3 Bucket') {
+        /* stage('Clean Up S3 Bucket') {
             steps {
-                withAWS(region: "${AWS_REGION}", credentials: '99e62274-16c1-482c-b2e7-e575ee38fbb1') {
+                withAWS(region: "${AWS_REGION}", credentials: '10d1d59f-ace4-44ed-8496-7998a8f7f71a') {
                     echo "Cleaning up S3 bucket: ${S3_BUCKET}"
-                    sh "aws s3 rm s3://${S3_BUCKET} --recursive"
+                    sh "aws s3 ls s3://${S3_BUCKET} --recursive"  // List contents before deletion for verification
+                    sh "aws s3 rm s3://${S3_BUCKET} --recursive" // Delete all files in the S3 
                 }
             }
         }
-/* 
+        
         stage('Build with CodeBuild') {
             steps {
                 script {
                     // Define variables
-                    def projectName = 'test'  // Replace with your CodeBuild project name
-                    def credentialsId = '99e62274-16c1-482c-b2e7-e575ee38fbb1'  // Replace with your Jenkins credentials ID
-                    def region = 'eu-north-1'  // Replace with your AWS region
+                    def projectName = "${CODEBUILD_PROJECT}"  // Use environment variable
+                    def credentialsId = '99e62274-16c1-482c-b2e7-e575ee38fbb1'  //  CodeBuild credentials ID
+                    def region = "${AWS_REGION}"  // Use environment variable
                     
                     // Start the AWS CodeBuild project
                     def result = awsCodeBuild(
                         projectName: projectName,
                         credentialsId: credentialsId,
                         region: region,
-                        credentialsType: 'jenkins',  // Use 'jenkins' for Jenkins credentials
-                        sourceControlType: 'project'  // Ensure this matches your CodeBuild source control settings
+                        credentialsType: 'jenkins',  // 'jenkins' for Jenkins credentials
+                        sourceControlType: 'project'  // 'project' for using the source of the Codebuild
                     )
                     
                     // Optional: Print build details
@@ -51,18 +52,32 @@ pipeline {
             }
         } */
 
-/* 
-        stage('Upload to S3') {
+        stage('Check S3 Bucket') {
             steps {
-                withAWS(region: "${AWS_REGION}", credentials: '99e62274-16c1-482c-b2e7-e575ee38fbb1') {
-                    sh "aws s3 cp / s3://${S3_BUCKET}/ --recursive"
+                script {
+                    def checkS3Files = sh(script: '''
+                        #!/bin/bash
+                        FILE_COUNT=$(aws s3 ls s3://${S3_BUCKET}/ --recursive | wc -l)
+                        if [ $FILE_COUNT -gt 0 ]; then
+                            echo "Files are uploaded to S3"
+                            echo "Number of files: $FILE_COUNT"
+                            exit 0
+                        else
+                            echo "Failed to Upload to S3"
+                            exit 1
+                        fi
+                    ''', returnStatus: true)
+
+                    if (checkS3Files != 0) {
+                        error "Failed to Upload to S3"
+                    }
                 }
             }
         }
+
+    
     }
-     */
-
-
+    
     post {
         success {
             echo 'Pipeline executed successfully!'
